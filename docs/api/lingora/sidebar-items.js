bdb9bb72cc17fb0d8c63d6e03e439ad9b547d1a7
Nigel Eke <nigeleke@users.noreply.github.com>
2025-02-07 01:53:14 +0000

    Release lingora@0.1.4

--- docs/api/lingora/sidebar-items.js
+++ docs/api/lingora/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["AppError","OutputMode"],"struct":["App","CommandLineArgs"]};
+window.SIDEBAR_ITEMS = {"enum":["AppError","OutputMode"],"struct":["App","CommandLineArgs","ResolvedArgs","ResolvedArgsBuilder"]};
---
+++ docs/api/trait.impl/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["lingora",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"lingora/struct.ResolvedArgsBuilder.html\" title=\"struct lingora::ResolvedArgsBuilder\">ResolvedArgsBuilder</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[309]}
