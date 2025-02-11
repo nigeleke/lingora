be782607039083532c227e3567a62655356725c7
Nigel Eke <nigeleke@users.noreply.github.com>
2025-02-11 11:44:23 +0000

    Release lingora@0.1.13

--- docs/api/trait.impl/core/marker/trait.Copy.js
+++ docs/api/trait.impl/core/marker/trait.Copy.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["lingora",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"lingora/enum.OutputMode.html\" title=\"enum lingora::OutputMode\">OutputMode</a>"]]]]);
+    var implementors = Object.fromEntries([["lingora",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"lingora/enum.OutputMode.html\" title=\"enum lingora::OutputMode\">OutputMode</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[265]}
+//{"start":57,"fragment_lengths":[266]}
