6a6d0b4c9819f7ea069a05cd8807b96a6a8c8d63
Nigel Eke <nigeleke@users.noreply.github.com>
2025-02-18 04:30:39 +0000

    Release lingora@0.1.15

--- docs/api/src-files.js
+++ docs/api/src-files.js
@@ -1,3 +1,3 @@
-var srcIndex = new Map(JSON.parse('[["lingora",["",[["config",[],["arguments.rs","interim_settings.rs","mod.rs","settings.rs"]],["domain",[],["analysis.rs","fluent_file.rs","integrity_check.rs","integrity_cross_check.rs","integrity_warning.rs","locale.rs","mod.rs"]],["output",[],["analysis_writer.rs","dioxis_i18n_config_writer.rs","error.rs","mod.rs","writer.rs"]]],["app.rs","lib.rs"]]]]'));
+var srcIndex = new Map(JSON.parse('[["lingora",["",[["config",[],["arguments.rs","interim_settings.rs","mod.rs","settings.rs"]],["domain",[],["analysis.rs","fluent_file.rs","identifier.rs","integrity_check.rs","integrity_checks.rs","integrity_cross_check.rs","integrity_warning.rs","locale.rs","mod.rs"]],["gui",[["components",[],["description.rs","identifiers.rs","languages.rs","mod.rs","scrollable.rs","status.rs","translation.rs","workspace.rs"]]],["app.rs","mod.rs","state.rs"]],["output",[],["analysis_writer.rs","dioxis_i18n_config_writer.rs","error.rs","mod.rs","writer.rs"]]],["app.rs","lib.rs","utils.rs"]]]]'));
 createSrcSidebar();
-//{"start":36,"fragment_lengths":[353]}
+//{"start":36,"fragment_lengths":[581]}
