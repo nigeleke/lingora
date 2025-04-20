8e041c779e175111fae96394d37c105fd5aa44f4
Nigel Eke <nigeleke@users.noreply.github.com>
2025-04-20 03:57:40 +0000

    Release lingora@0.1.40

---
+++ docs/api/trait.impl/core/marker/trait.UnsafeUnpin.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["lingora",[["impl UnsafeUnpin for <a class=\"enum\" href=\"lingora/enum.AppError.html\" title=\"enum lingora::AppError\">AppError</a>",1,["lingora::app::AppError"]],["impl UnsafeUnpin for <a class=\"enum\" href=\"lingora/enum.OutputMode.html\" title=\"enum lingora::OutputMode\">OutputMode</a>",1,["lingora::config::arguments::OutputMode"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"lingora/struct.App.html\" title=\"struct lingora::App\">App</a>",1,["lingora::app::App"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"lingora/struct.Arguments.html\" title=\"struct lingora::Arguments\">Arguments</a>",1,["lingora::config::arguments::Arguments"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[661]}
