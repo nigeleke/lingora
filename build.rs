dd069b4dbc67e41a0363162c02f5d0f619c3d5f3
Nigel Eke <nigeleke@gmail.com>
2025-09-04 23:29:42 +1000

    Set env LANG & LC_ALL during build

--- build.rs
+++ build.rs
@@ -1,8 +1,4 @@
-use std::env;
-
 fn main() {
-    if env::var("CARGO_CFG_TEST").is_ok() {
-        println!("cargo:rustc-env=LANG=en_US.UTF-8");
-        println!("cargo:rustc-env=LC_ALL=en_US.UTF-8");
-    }
+    println!("cargo::rustc-env=LANG=en_US.UTF-8");
+    println!("cargo::rustc-env=LC_ALL=en_US.UTF-8");
 }
